c5ebf68f751da9c279ae86ed70b335f1a64d6c67
Encarnacion Cituk Caamal <programaciondyn3.mx@sandos.com>
2019-08-29 14:42:09 -0500

    login con tokens con angular

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,103 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UsuarioModel } from '../models/usuario.model';
+import { map } from 'rxjs/operators';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private url = 'https://identitytoolkit.googleapis.com/v1/accounts:';
+  private apiKey = 'AIzaSyCe1EBq297tO5ibt63c2GKTehqrmzbhJOM';
+
+  userToken: string;
+
+  constructor(private http: HttpClient) {
+    this.leerToken();
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+
+  login(usuario: UsuarioModel) {
+
+    const authData = {
+      ...usuario,
+      returnSecureToken: true
+    };
+
+    return this.http.post(
+      `${this.url}signInWithPassword?key=${this.apiKey}`,
+      authData
+    ).pipe(
+      map(respuesta => {
+        this.guardarToken(respuesta['idToken']);
+        return respuesta;
+      })
+    );
+  }
+
+
+  nuevoUsuario(usuario: UsuarioModel) {
+    const authData = {
+      // email: usuario.email,
+      // password: usuario.password,
+      ...usuario,
+      returnSecureToken: true
+    };
+
+    return this.http.post(
+      `${this.url}signUp?key=${this.apiKey}`,
+      authData
+    ).pipe(
+      map( respuesta => {
+        this.guardarToken( respuesta['idToken'] );
+        return respuesta;
+      })
+    );
+
+  }
+
+  private guardarToken( idToken: string) {
+
+    this.userToken = idToken;
+    localStorage.setItem('token', idToken);
+
+    const hoy = new Date();
+    hoy.setSeconds( 3600 );
+
+    localStorage.setItem('expira', hoy.getTime().toString());
+
+  }
+
+  leerToken() {
+
+    if (localStorage.getItem('token')) {
+      this.userToken = localStorage.getItem('token');
+    } else {
+      this.userToken = '';
+    }
+
+    return this.userToken;
+
+  }
+
+  estaAutenticado(): boolean {
+
+    if ( this.userToken.length < 2 ) {
+      return false;
+    }
+    const expira = Number(localStorage.getItem('expira'));
+    const expiraDate = new Date();
+    expiraDate.setTime(expira);
+
+    if (expiraDate > new Date() ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
